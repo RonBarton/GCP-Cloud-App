b432d14943aca4fb30df2fcc6e4065ff5c8c7e23
Ron B <r.barton179@gmail.com>
2020-02-02 14:29:21 -0500

    final nlp doesn't work still1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 import pandas as pd
+import wikipedia
 
 app = Flask(__name__)
 
@@ -12,13 +13,29 @@ def hello():
 
 @app.route('/bob')
 def bob():
-    val = {"value": "Luke is a gheyfish"}
+    val = {"value": "bob"}
     return jsonify(val)
 
 @app.route('/pandas')
 def pandas_sugar():
     df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
     return jsonify(df.to_dict())
+
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+
+    # Imports the Google Cloud client library
+    from google.cloud import language
+    from google.cloud.language import enums
+    from google.cloud.language import types
+    result = wikipedia.summary(company, sentences=10)
+
+    client = language.LanguageServiceClient()
+    document = types.Document(
+        content=result,
+        type=enums.Document.Type.PLAIN_TEXT)
+    entities = client.analyze_entities(document).entities
+    return str(entities)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
